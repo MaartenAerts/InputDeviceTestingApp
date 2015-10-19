2a4921a22ff5aee7acbb808e28e47ff64c19e903
Slein04 <nielsvervliet04@hotmail.com>
2015-10-19 13:25:32 +0200

    testingTests

---
+++ src/main/java/org/uantwerpen/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package org.uantwerpen;
+
+/**
+ * Created by Niels on 19/10/2015.
+ */
+public class ApplicationConfig {
+}
---
+++ src/test/java/InputDeviceTesting/ApplicationTest.java
@@ -0,0 +1,7 @@
+package InputDeviceTesting;
+
+/**
+ * Created by Niels on 19/10/2015.
+ */
+public class ApplicationTest {
+}
