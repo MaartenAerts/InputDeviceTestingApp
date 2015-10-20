16c0e1d5c9e04c773621e434758ac0366a8a5226
Slein04 <nielsvervliet04@hotmail.com>
2015-10-20 13:54:55 +0200

    ErrorsGefixed2

--- src/main/java/org/uantwerpen/web/ResearcherController.java
+++ src/main/java/org/uantwerpen/web/ResearcherController.java
@@ -1,5 +1,9 @@
 package org.uantwerpen.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ResearcherController {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello, world";
+    }
 }
