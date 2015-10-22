23019ff5968f552f55b8ffd636787ff43047730f
Maarten Aerts <maarten.702@gmail.com>
2015-10-22 09:10:27 +0200

    React code updated to v0.14

--- src/main/webapp/resources/ts/test.tsx
+++ src/main/webapp/resources/ts/test.tsx
@@ -1,14 +1,11 @@
 /// <reference path="../typings/react/react.d.ts" />
-/// <reference path="../typings/requirejs/require.d.ts" />
 ///<reference path="../typings/react-dom/react-dom.d.ts"/>
-
-import React = require('react')
-
-
 
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
-class Test extends React.Component {
+
+class Test extends React.Component<propke,stateje>
+{
     constructor(props) {
         super(props);
 
@@ -24,116 +21,135 @@ class Test extends React.Component {
         let cxBigCircle = this.props.width / 2;
         let cyBigCircle = this.props.height / 2;
 
+        /*   var circles = new Array();
+         var circle = function(id, x, y, radius) {
+         this.left = x - radius;
+         this.top = y - radius;
+         this.right = x + radius;
+         this.bottom = y + radius;
+         };*/
+
         let radius = radiusBigCircle/10;
 
-        let aantalCirkels = 25;
+        let aantalCirkels = 15;
         let graden = 360/aantalCirkels;
         let xCircleArray = new Array();
         let yCircleArray = new Array();
 
-        let teller = 0;
+        let tellerVoorHelft = 0;
+        let tellerNaHelft = 0;
         let circleID = 0;
+        let IdArray = new Array();
 
         for(var i = 1; i <= aantalCirkels;i++) {
-            let radian = (graden*(i-1))*(Math.PI/180);
-            xCircleArray[i] = radiusBigCircle * Math.cos(radian);
-            yCircleArray[i] = radiusBigCircle * Math.sin(radian);
+            let radian = (graden * (i - 1)) * (Math.PI / 180);
+            xCircleArray[i] = radiusBigCircle * Math.cos(radian) + cxBigCircle;
+            yCircleArray[i] = radiusBigCircle * Math.sin(radian) + cyBigCircle;
+            // let xCircle = radiusBigCircle * Math.cos(radian) + cxBigCircle;
+            // let yCircle = radiusBigCircle * Math.sin(radian) + cyBigCircle;
 
-            circleID = (teller+1) + i;
-            teller++;
+            if (i <= ((aantalCirkels + 1) / 2)) {
+                circleID = tellerVoorHelft + 1;
+                tellerVoorHelft += 2;
+            } else {
+                circleID = tellerNaHelft + 2;
+                tellerNaHelft += 2;
+            }
+            IdArray[i] = circleID;
+
+            /*     var circle = new circle(circleID,xCircle,yCircle,radius);
+             circles.push(circle);
+             circles[i].Id = circleID;
+             }
+
+             {this.renderCircle(circles[1].id, circles[1].x, circles[1].y,radius,strokeWidthBigCircle)}
+             */
         }
-/*
-        // degrees naar radians omzetten
-        let graden2 = 36;
-        let radian2 = graden2 * (Math.PI/180);
-        let moveX2 = radiusBigCircle * Math.cos(radian2);         //cirkel 2
-        let moveY2 = radiusBigCircle * Math.sin(radian2);         //cirkel 2
 
-        let graden3 = 72;
-        let radian3 = graden3 * (Math.PI/180);
-        let moveX3 = radiusBigCircle * Math.cos(radian3);         //cirkel 3
-        let moveY3 = radiusBigCircle * Math.sin(radian3);         //cirkel 3
+        var clickedX = event.pageX;
+        var clickedY = event.pageY;
 
-        let moveX4 = radiusBigCircle * Math.cos((graden3+36) * (Math.PI/180))           //cirkel 4
-        let moveY4 = radiusBigCircle * Math.sin((graden3+36) * (Math.PI/180))           //cirkel 4
+        for (var i = 1; i < 15+1; i++) {
+            if (clickedX < xCircleArray[i]+radius){ //&& clickedX > xCircleArray[i]-radius && clickedY > yCircleArray[i]-radius && clickedY < yCircleArray[i]+radius) {
+                alert ('clicked number ' + (i));
+            }
+        }
 
-        let moveX5 = radiusBigCircle * Math.cos((graden3+72) * (Math.PI/180))           //cirkel 5
-        let moveY5 = radiusBigCircle * Math.sin((graden3+72) * (Math.PI/180))           //cirkel 5
-
-        let moveX6 = radiusBigCircle * Math.cos((graden3+108) * (Math.PI/180))           //cirkel 6
-        let moveY6 = radiusBigCircle * Math.sin((graden3+108) * (Math.PI/180))           //cirkel 6
-
-        let moveX7 = radiusBigCircle * Math.cos((graden3+144) * (Math.PI/180))           //cirkel 7
-        let moveY7 = radiusBigCircle * Math.sin((graden3+144) * (Math.PI/180))           //cirkel 7
-
-        let moveX8 = radiusBigCircle * Math.cos((graden3+180) * (Math.PI/180))           //cirkel 8
-        let moveY8 = radiusBigCircle * Math.sin((graden3+180) * (Math.PI/180))           //cirkel 8
-
-        let moveX9 = radiusBigCircle * Math.cos((graden3+216) * (Math.PI/180))           //cirkel 9
-        let moveY9 = radiusBigCircle * Math.sin((graden3+216) * (Math.PI/180))           //cirkel 9
-
-        let moveX10 = radiusBigCircle * Math.cos((graden3+252) * (Math.PI/180))           //cirkel 10
-        let moveY10 = radiusBigCircle * Math.sin((graden3+252) * (Math.PI/180))           //cirkel 10
-*/
-
-        /*
-        * {this.renderCircle(cxBigCircle + radiusBigCircle,cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX2 + cxBigCircle,moveY2 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX3 + cxBigCircle,moveY3 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX4 + cxBigCircle,moveY4 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX5 + cxBigCircle,moveY5 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX6 + cxBigCircle,moveY6 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX7 + cxBigCircle,moveY7 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX8 + cxBigCircle,moveY8 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX9 + cxBigCircle,moveY9 + cyBigCircle,radius,strokeWidthBigCircle)}
-         {this.renderCircle(moveX10 + cxBigCircle,moveY10 + cyBigCircle,radius,strokeWidthBigCircle)}*/
-
-        return <svg ref="svg" {...this.props}>
+        return <svg ref="svg" {...this.props} onMouseMove={this.handleMouseMove.bind(this)}>
             {this.renderBigCircle(cxBigCircle,cyBigCircle,radiusBigCircle,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[1] + cxBigCircle, yCircleArray[1] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[2] + cxBigCircle, yCircleArray[2] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[3] + cxBigCircle, yCircleArray[3] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[4] + cxBigCircle, yCircleArray[4] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[5] + cxBigCircle, yCircleArray[5] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[6] + cxBigCircle, yCircleArray[6] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[7] + cxBigCircle, yCircleArray[7] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[8] + cxBigCircle, yCircleArray[8] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[9] + cxBigCircle, yCircleArray[9] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[10] + cxBigCircle, yCircleArray[10] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[11] + cxBigCircle, yCircleArray[11] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[12] + cxBigCircle, yCircleArray[12] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[13] + cxBigCircle, yCircleArray[13] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[14] + cxBigCircle, yCircleArray[14] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[15] + cxBigCircle, yCircleArray[15] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[16] + cxBigCircle, yCircleArray[16] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[17] + cxBigCircle, yCircleArray[17] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[18] + cxBigCircle, yCircleArray[18] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[19] + cxBigCircle, yCircleArray[19] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[20] + cxBigCircle, yCircleArray[20] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[21] + cxBigCircle, yCircleArray[21] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[22] + cxBigCircle, yCircleArray[22] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[23] + cxBigCircle, yCircleArray[23] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[24] + cxBigCircle, yCircleArray[24] + cyBigCircle,radius,strokeWidthBigCircle)}
-            {this.renderCircle(xCircleArray[25] + cxBigCircle, yCircleArray[25] + cyBigCircle,radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[1], xCircleArray[1], yCircleArray[1],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[2], xCircleArray[2], yCircleArray[2],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[3], xCircleArray[3], yCircleArray[3],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[4], xCircleArray[4], yCircleArray[4],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[5], xCircleArray[5], yCircleArray[5],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[6], xCircleArray[6], yCircleArray[6],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[7], xCircleArray[7], yCircleArray[7],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[8], xCircleArray[8], yCircleArray[8],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[9], xCircleArray[9], yCircleArray[9],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[10], xCircleArray[10], yCircleArray[10],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[11], xCircleArray[11], yCircleArray[11],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[12], xCircleArray[12], yCircleArray[12],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[13], xCircleArray[13], yCircleArray[13],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[14], xCircleArray[14], yCircleArray[14],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[15], xCircleArray[15], yCircleArray[15],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[16], xCircleArray[16], yCircleArray[16],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[17], xCircleArray[17], yCircleArray[17],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[18], xCircleArray[18], yCircleArray[18],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[19], xCircleArray[19], yCircleArray[19],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[20], xCircleArray[20], yCircleArray[20],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[21], xCircleArray[21], yCircleArray[21],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[22], xCircleArray[22], yCircleArray[22],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[23], xCircleArray[23], yCircleArray[23],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[24], xCircleArray[24], yCircleArray[24],radius,strokeWidthBigCircle)}
+            {this.renderCircle(IdArray[25], xCircleArray[25], yCircleArray[25],radius,strokeWidthBigCircle)}
+
         </svg>;
     }
 
-ReactDom.render(<HelloMessage name="John" />,    document.getElementById('example'));
-    renderBigCircle(cxBigCirle, cyBigCircle, radiusBigCircle, strokewidthBigCircle) {
-        return <circle cx={cxBigCirle} cy={cyBigCircle} r={radiusBigCircle-10} fill="#ccffff" stroke="black" strokeWidth={"1"}/>;
+    renderBigCircle(cxBigCircle, cyBigCircle, radiusBigCircle, strokewidthBigCircle) {
+        return <circle cx={cxBigCircle} cy={cyBigCircle} r={radiusBigCircle-10} fill="#ccffff" stroke="black" strokeWidth={"1"}/>;
     }
 
-    renderCircle(cx, cy, radius, strokewidth) {
-        return <circle cx={cx} cy={cy} r={radius} fill="red" stroke="black" strokeWidth={strokewidth}  />;
+    renderCircle(id, cx, cy, radius, strokewidth) {
+        /*var circle = function() {
+         this.left = cx - radius;
+         this.top =  cy - radius;
+         this.right = cx + radius;
+         this.bottom = cy + radius;
+         }*/
+        //  circles.push(circle());
+        return <circle id={id} cx={cx} cy={cy} r={radius} fill="red" stroke="black" strokeWidth={strokewidth} />;
+    }
+
+    /*updateCircle(id, cx,cy,radius, strokewidth, color) {
+     return <circle id={id} cx={cx} cy={cy} r={radius} fill={color} stroke="black" strokeWidth={strokewidth} />;
+     }*/
+
+
+    handleMouseMove (event) {
+        // let svg = React.findDOMNode(this.refs.svg);
+        //   let rect = svg.getBoundingClientRect();
+        /* this.setState({
+         x: event.clientX,
+         y: event.clientY
+         });*/
+
+
+
+        /*    let clickedX, clickedY;
+         onclick(function(e){
+         clickedX = this.state.x;
+         clickedY = this.state.y;
+         });*/
     }
 
 }
-
 ReactDom.render(
-        <Test width="900" height="600" />,
-        document.getElementById('canvas_for_test')
-    );
+    <Test width="900" height="600" />,
+    document.getElementById('canvas_for_test')
 
+
+);
 
 
 
