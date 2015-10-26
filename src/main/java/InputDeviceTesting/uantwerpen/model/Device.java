f94cae5c7a61b817bdddc314b89828171a3293cc
Maarten Aerts <maarten.702@gmail.com>
2015-10-26 16:18:55 +0100

    Researcher alles in orde, device id error fixed

--- src/main/java/InputDeviceTesting/uantwerpen/model/Device.java
+++ src/main/java/InputDeviceTesting/uantwerpen/model/Device.java
@@ -1,10 +1,10 @@
 package InputDeviceTesting.uantwerpen.model;
 
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
