f5e1f645ca1079c1a1ed1f390ede3d2464177482
Maarten Aerts <maarten.702@gmail.com>
2015-11-05 10:46:29 +0100

    overstap naar thymeleaf

---
+++ src/main/resources/static/js/testjoris.js
@@ -0,0 +1,93 @@
+/// <reference path="../typings/react/react.d.ts" />
+///<reference path="../typings/react-dom/react-dom.d.ts"/>
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+define(["require", "exports", 'react', 'react-with-addons', 'react-dom'], function (require, exports, React, ReactAddons, ReactDom) {
+    var Login = (function (_super) {
+        __extends(Login, _super);
+        function Login(props) {
+            _super.call(this, props);
+            this.state =
+                {
+                    username: '',
+                    password: ''
+                };
+        }
+        return Login;
+    })(React.Component);
+    /*
+    var LoginForm = React.createClass({
+        mixins: [ReactAddons.addons.LinkedStateMixin],
+    
+        getInitialState:function()
+        {return{username: 'leeg', password:'leeg'};
+        },
+    
+    
+        render: function()
+        {
+            return(
+                <div className="login jumbotron center-block">
+                    <h1>Login</h1>
+                    <form role="form">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text"  valueLink={this.linkState('username')} className="form-control" id="username" placeholder="Username" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" valueLink={this.linkState('password')} className="form-control" id="password" ref="password" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="btn btn-default" onClick={this.login.bind(this)}>Submit</button>
+                    </form>
+                </div>
+            );
+        }
+    
+    });
+    */
+    //class HelloMessage extends React.Component<HelloWorldProps, {}>
+    //{
+    //    render()
+    //    {
+    //        return(
+    //            <h2>i like to eat</h2>
+    //        );
+    //    }
+    //}
+    //
+    //ReactDom.render
+    //(
+    //    <HelloMessage name="John" />,
+    //    document.getElementById('example')
+    //
+    //);
+    var Form = React.createClass({
+        mixins: [ReactAddons.addons.LinkedStateMixin],
+        getInitialState: function () {
+            return { text: "test", password: "testje" };
+        },
+        clickmij: function (event) {
+            event.currentTarget.style.backgroundColor = '#000000';
+        },
+        render: function () {
+            return (React.createElement("div", {"className": "login jumbotron center-block"}, React.createElement("h1", null, "Login"), React.createElement("form", {"role": "form"}, React.createElement("div", {"className": "form-group"}, React.createElement("label", {"htmlFor": "username"}, "Username"), React.createElement("input", {"type": "text", "valueLink": this.linkState("text"), "className": "form-control", "id": "username", "placeholder": "Username"})), React.createElement("div", {"className": "form-group"}, React.createElement("label", {"htmlFor": "password"}, "Password"), React.createElement("input", {"type": "password", "valueLink": this.linkState("password"), "className": "form-control", "id": "password", "ref": "password", "placeholder": "Password"}), React.createElement("button", {"type": "button", "className": "btn btn-default", "onClick": this.clickmij}, "Submit")))));
+        }
+    });
+    ReactDom.render(React.createElement(Form, null), document.getElementById("user"));
+    var Button = (function (_super) {
+        __extends(Button, _super);
+        function Button() {
+            _super.apply(this, arguments);
+        }
+        Button.prototype.render = function () {
+            return (React.createElement("button", {"type": "submit", "className": "btn btn-default"}, "Submit"));
+        };
+        return Button;
+    })(React.Component);
+    ReactDom.render(React.createElement(Button, {"name": "John"}), document.getElementById('hier'));
+});
+//# sourceMappingURL=testjoris.js.map
