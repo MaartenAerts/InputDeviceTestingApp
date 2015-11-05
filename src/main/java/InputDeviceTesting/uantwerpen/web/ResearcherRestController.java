47e7531d15e0dc980ea238beee6a2168f95cffb2
Maarten Aerts <maarten.702@gmail.com>
2015-11-05 11:33:30 +0100

    overstap naar thymeleaf

--- src/main/java/InputDeviceTesting/uantwerpen/web/ResearcherRestController.java
+++ src/main/java/InputDeviceTesting/uantwerpen/web/ResearcherRestController.java
@@ -1,130 +1,130 @@
-package InputDeviceTesting.uantwerpen.web;
-
-import InputDeviceTesting.uantwerpen.model.Researcher;
-import InputDeviceTesting.uantwerpen.repo.ResearcherRepo;
-import InputDeviceTesting.uantwerpen.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
-
-
-/**
- * Created by TooLate on 16/10/2015.
- */
-
-@RestController
-@RequestMapping("/api/researcher/")
-public class ResearcherRestController {
-    @Autowired
-    private ResearcherRepo researcherRepo;
-
-
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Researcher> findAll(){
-        return (researcherRepo.findAll());
-    }
-
-    //Retrieve single researcher
-    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
-    public ResponseEntity<Researcher> getResearcher(@PathVariable("id") long id) {
-
-        System.out.println("Retrieving researcher with id " + id);
-        Researcher researcher = researcherRepo.findById(id);
-        if(researcher == null){
-            System.out.println("Researcher with id " + id + " not found");
-            return new ResponseEntity<Researcher>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<Researcher>(researcher, HttpStatus.OK);
-    }
-
-    //Create researcher
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> createResearcher(@RequestBody Researcher researcher,    UriComponentsBuilder ucBuilder) {
-
-        System.out.println("Creating researcher " + researcher.getEmail());
-
-        if (researcherRepo.exists(researcher.getId())){
-            System.out.println("A researcher with id: " + researcher.getId() + " email: " + researcher.getEmail() + " already exist");
-            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-        }
-        else if (researcherRepo.findByEmail(researcher.getEmail()) != null){
-            System.out.println("A researcher with id: " + researcher.getId() + " email: " + researcher.getEmail() + " already exist");
-            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-        }
-
-        researcherRepo.save(researcher);
-        System.out.println(researcher.toString() + " created!!!");
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ucBuilder.path("/{id}").buildAndExpand(researcher.getId()).toUri());
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
-        //return new ResponseEntity<Researcher>(researcher,HttpStatus.CREATED);
-    }
-
-    //Update researcher
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Researcher> updateResearcher(@PathVariable("id") long id, @RequestBody Researcher researcher) {
-
-        System.out.println("Updating researcher " + id);
-        Researcher currentResearcher = researcherRepo.findById(id);
-
-        if (currentResearcher==null) {
-            System.out.println("Researcher with id " + id + " not found");
-            return new ResponseEntity<Researcher>(HttpStatus.NOT_FOUND);
-        }
-
-        currentResearcher.setEmail(researcher.getEmail());
-        currentResearcher.setPassword(researcher.getPassword());
-        currentResearcher.setFirstName(researcher.getFirstName());
-
-        researcherRepo.save(currentResearcher);
-
-        return new ResponseEntity<Researcher>(currentResearcher, HttpStatus.OK);
-    }
-
-    //Delete researcher
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Researcher> deleteResearcher(@PathVariable("id") long id) {
-        System.out.println("Fetching & Deleting User with id " + id);
-
-        Researcher researcher = researcherRepo.findById(id);
-        if (researcher == null) {
-            System.out.println("Unable to delete. Researcher with id " + id + " not found");
-            return new ResponseEntity<Researcher>(HttpStatus.NOT_FOUND);
-        }
-
-        researcherRepo.delete(researcher);
-        return new ResponseEntity<Researcher>(HttpStatus.NO_CONTENT);
-    }
-
-    //Delete all researchers
-    @RequestMapping(value = "/deleteAll", method = RequestMethod.DELETE)
-    public ResponseEntity<Researcher> deleteAllResearchers() {
-        System.out.println("Deleting All Researchers");
-
-        researcherRepo.deleteAll();
-        return new ResponseEntity<Researcher>(HttpStatus.NO_CONTENT);
-    }
-
-    /*@RequestMapping(params={"save"},method = RequestMethod.POST)
-    public String save(@RequestBody Researcher researcher){ //Save , add , insert, update = hetzelfde.
-        customUserDetailsService.save(researcher);
-        return "addResearcher";
-    }*/
-
-    @RequestMapping(value = "hello",method=RequestMethod.GET)
-    public String hello(){
-        return "YOLO";
-    }
-
-    private Researcher generateResearcher(int j){
-        Researcher researcher = new Researcher("mail@mail.com","pass");
-        return researcher;
-    }
-
-}
+//package InputDeviceTesting.uantwerpen.web;
+//
+//import InputDeviceTesting.uantwerpen.model.Researcher;
+//import InputDeviceTesting.uantwerpen.repo.ResearcherRepo;
+//import InputDeviceTesting.uantwerpen.service.CustomUserDetailsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.util.UriComponentsBuilder;
+//
+//import java.util.List;
+//
+//
+///**
+// * Created by TooLate on 16/10/2015.
+// */
+//
+//@RestController
+//@RequestMapping("/api/researcher/")
+//public class ResearcherRestController {
+//    @Autowired
+//    private ResearcherRepo researcherRepo;
+//
+//
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<Researcher> findAll(){
+//        return (researcherRepo.findAll());
+//    }
+//
+//    //Retrieve single researcher
+//    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+//    public ResponseEntity<Researcher> getResearcher(@PathVariable("id") long id) {
+//
+//        System.out.println("Retrieving researcher with id " + id);
+//        Researcher researcher = researcherRepo.findById(id);
+//        if(researcher == null){
+//            System.out.println("Researcher with id " + id + " not found");
+//            return new ResponseEntity<Researcher>(HttpStatus.NOT_FOUND);
+//        }
+//        return new ResponseEntity<Researcher>(researcher, HttpStatus.OK);
+//    }
+//
+//    //Create researcher
+//    @RequestMapping(method = RequestMethod.POST)
+//    public ResponseEntity<Void> createResearcher(@RequestBody Researcher researcher,    UriComponentsBuilder ucBuilder) {
+//
+//        System.out.println("Creating researcher " + researcher.getEmail());
+//
+//        if (researcherRepo.exists(researcher.getId())){
+//            System.out.println("A researcher with id: " + researcher.getId() + " email: " + researcher.getEmail() + " already exist");
+//            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+//        }
+//        else if (researcherRepo.findByEmail(researcher.getEmail()) != null){
+//            System.out.println("A researcher with id: " + researcher.getId() + " email: " + researcher.getEmail() + " already exist");
+//            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+//        }
+//
+//        researcherRepo.save(researcher);
+//        System.out.println(researcher.toString() + " created!!!");
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setLocation(ucBuilder.path("/{id}").buildAndExpand(researcher.getId()).toUri());
+//        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+//        //return new ResponseEntity<Researcher>(researcher,HttpStatus.CREATED);
+//    }
+//
+//    //Update researcher
+//    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+//    public ResponseEntity<Researcher> updateResearcher(@PathVariable("id") long id, @RequestBody Researcher researcher) {
+//
+//        System.out.println("Updating researcher " + id);
+//        Researcher currentResearcher = researcherRepo.findById(id);
+//
+//        if (currentResearcher==null) {
+//            System.out.println("Researcher with id " + id + " not found");
+//            return new ResponseEntity<Researcher>(HttpStatus.NOT_FOUND);
+//        }
+//
+//        currentResearcher.setEmail(researcher.getEmail());
+//        currentResearcher.setPassword(researcher.getPassword());
+//        currentResearcher.setFirstName(researcher.getFirstName());
+//
+//        researcherRepo.save(currentResearcher);
+//
+//        return new ResponseEntity<Researcher>(currentResearcher, HttpStatus.OK);
+//    }
+//
+//    //Delete researcher
+//    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+//    public ResponseEntity<Researcher> deleteResearcher(@PathVariable("id") long id) {
+//        System.out.println("Fetching & Deleting User with id " + id);
+//
+//        Researcher researcher = researcherRepo.findById(id);
+//        if (researcher == null) {
+//            System.out.println("Unable to delete. Researcher with id " + id + " not found");
+//            return new ResponseEntity<Researcher>(HttpStatus.NOT_FOUND);
+//        }
+//
+//        researcherRepo.delete(researcher);
+//        return new ResponseEntity<Researcher>(HttpStatus.NO_CONTENT);
+//    }
+//
+//    //Delete all researchers
+//    @RequestMapping(value = "/deleteAll", method = RequestMethod.DELETE)
+//    public ResponseEntity<Researcher> deleteAllResearchers() {
+//        System.out.println("Deleting All Researchers");
+//
+//        researcherRepo.deleteAll();
+//        return new ResponseEntity<Researcher>(HttpStatus.NO_CONTENT);
+//    }
+//
+//    /*@RequestMapping(params={"save"},method = RequestMethod.POST)
+//    public String save(@RequestBody Researcher researcher){ //Save , add , insert, update = hetzelfde.
+//        customUserDetailsService.save(researcher);
+//        return "addResearcher";
+//    }*/
+//
+//    @RequestMapping(value = "hello",method=RequestMethod.GET)
+//    public String hello(){
+//        return "YOLO";
+//    }
+//
+//    private Researcher generateResearcher(int j){
+//        Researcher researcher = new Researcher("mail@mail.com","pass");
+//        return researcher;
+//    }
+//
+//}
--- src/test/java/InputDeviceTesting/web/ResearcherControllerTests.java
+++ src/test/java/InputDeviceTesting/web/ResearcherControllerTests.java
@@ -1,112 +1,112 @@
-package InputDeviceTesting.web;
-
-import InputDeviceTesting.InputDeviceTestingAppApplication;
-import InputDeviceTesting.uantwerpen.model.Researcher;
-import InputDeviceTesting.uantwerpen.repo.ResearcherRepo;
-import InputDeviceTesting.uantwerpen.web.ResearcherRestController;
-import org.apache.http.client.HttpClient;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.nio.charset.Charset;
-
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import com.google.gson.Gson;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.CoreMatchers.is;
-
-
-/**
- * Created by Niels on 30/10/2015.
- */
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = InputDeviceTestingAppApplication.class)
-@WebAppConfiguration
-public class ResearcherControllerTests {
-
-    private MockMvc mockMvc;
-
-    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
-            MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
-    @Autowired
-    private ResearcherRepo researcherRepo;
-
-    @Autowired
-    private ResearcherRestController researcherRestController;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    private Researcher researcher;
-    private Gson gson = new Gson();
-    String json;
-    String serviceURL = "http://localhost:8086/api/researcher/";
-    HttpClient httpClient;
-
-    @Before
-    public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(researcherRestController)
-                .build();
-    }
-
-   /* @Test
-    public void findResearchers() throws Exception{
-
-        this.mockMvc
-                .perform(
-                        get
-
-    }*/
-
-    @Test
-    public void testSaveResearcher() throws Exception  {
-
-        // create our object as a json string
-        researcher = new Researcher("trol@mail.com","trol");
-            json = gson.toJson(researcher);
-        System.out.println(json);
-
-        // create our object as a json string
-        //researcher = new Researcher("potzak@mail.com","pot");
-        //    json = gson.toJson(researcher);
-        //System.out.println(json);
-
-        this.mockMvc
-                .perform(
-                        post("/api/researcher/").content(json)
-                                .contentType(APPLICATION_JSON_UTF8))
-                        .andExpect(status().isCreated());
-    }
-
-    @Test
-    public void testResearcherAlreadyExists() throws Exception {
-        // create our object as a json string
-        researcher = new Researcher("lol@brol.fu","trolol"); // email bestaat al en zou conflicten moeten geven.
-        json = gson.toJson(researcher);
-        System.out.println(json);
-
-        this.mockMvc
-                .perform(
-                        post("/api/researcher/").content(json)
-                                .contentType(APPLICATION_JSON_UTF8))
-                .andExpect(status().isConflict());
-
-    }
-}
-
-
+//package InputDeviceTesting.web;
+//
+//import InputDeviceTesting.InputDeviceTestingAppApplication;
+//import InputDeviceTesting.uantwerpen.model.Researcher;
+//import InputDeviceTesting.uantwerpen.repo.ResearcherRepo;
+//import InputDeviceTesting.uantwerpen.web.ResearcherRestController;
+//import org.apache.http.client.HttpClient;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.nio.charset.Charset;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import com.google.gson.Gson;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.hamcrest.CoreMatchers.is;
+//
+//
+///**
+// * Created by Niels on 30/10/2015.
+// */
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = InputDeviceTestingAppApplication.class)
+//@WebAppConfiguration
+//public class ResearcherControllerTests {
+//
+//    private MockMvc mockMvc;
+//
+//    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
+//            MediaType.APPLICATION_JSON.getType(),
+//            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+//
+//    @Autowired
+//    private ResearcherRepo researcherRepo;
+//
+//    @Autowired
+//    private ResearcherRestController researcherRestController;
+//
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
+//
+//    private Researcher researcher;
+//    private Gson gson = new Gson();
+//    String json;
+//    String serviceURL = "http://localhost:8086/api/researcher/";
+//    HttpClient httpClient;
+//
+//    @Before
+//    public void setup() throws Exception {
+//        this.mockMvc = MockMvcBuilders.standaloneSetup(researcherRestController)
+//                .build();
+//    }
+//
+//   /* @Test
+//    public void findResearchers() throws Exception{
+//
+//        this.mockMvc
+//                .perform(
+//                        get
+//
+//    }*/
+//
+//    @Test
+//    public void testSaveResearcher() throws Exception  {
+//
+//        // create our object as a json string
+//        researcher = new Researcher("trol@mail.com","trol");
+//            json = gson.toJson(researcher);
+//        System.out.println(json);
+//
+//        // create our object as a json string
+//        //researcher = new Researcher("potzak@mail.com","pot");
+//        //    json = gson.toJson(researcher);
+//        //System.out.println(json);
+//
+//        this.mockMvc
+//                .perform(
+//                        post("/api/researcher/").content(json)
+//                                .contentType(APPLICATION_JSON_UTF8))
+//                        .andExpect(status().isCreated());
+//    }
+//
+//    @Test
+//    public void testResearcherAlreadyExists() throws Exception {
+//        // create our object as a json string
+//        researcher = new Researcher("lol@brol.fu","trolol"); // email bestaat al en zou conflicten moeten geven.
+//        json = gson.toJson(researcher);
+//        System.out.println(json);
+//
+//        this.mockMvc
+//                .perform(
+//                        post("/api/researcher/").content(json)
+//                                .contentType(APPLICATION_JSON_UTF8))
+//                .andExpect(status().isConflict());
+//
+//    }
+//}
+//
+//
