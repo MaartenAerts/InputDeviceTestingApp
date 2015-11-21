b7f43d6ea72ebc8e69011197c36de69ca4602ed5
JoelGr <joelgrunhut@gmail.com>
2015-11-22 00:43:25 +0100

    Velden createTest aangepast in HTML

--- src/main/java/InputDeviceTesting/uantwerpen/web/TestController.java
+++ src/main/java/InputDeviceTesting/uantwerpen/web/TestController.java
@@ -1,6 +1,10 @@
 package InputDeviceTesting.uantwerpen.web;
 
+import InputDeviceTesting.uantwerpen.model.Test;
+import InputDeviceTesting.uantwerpen.repo.TestRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +16,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/test/")
 public class TestController {
+    @Autowired
+    TestRepo testRepo;
+
     @RequestMapping(method=RequestMethod.GET)//Todo:moet {id} worden
     public ModelAndView getTest(){
         return new ModelAndView("test");
     }
 
-    @RequestMapping("create")
+    @RequestMapping(value = "create", method = RequestMethod.GET)
     public ModelAndView createTest(){
         return new ModelAndView("createTest");
+    }
+    @RequestMapping(value="create", method=RequestMethod.POST)
+    public ModelAndView createTest(@ModelAttribute("testForm") Test test){
+        testRepo.save(test);
+        return new ModelAndView();
     }
 
 
