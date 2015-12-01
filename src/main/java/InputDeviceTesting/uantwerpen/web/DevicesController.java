a3cd47b7580414eed35a2206bcf08f93fe697b48
joris stuer <jorisstuer1@gmail.com>
2015-12-01 21:38:44 +0100

    start devices

---
+++ src/main/java/InputDeviceTesting/uantwerpen/web/DevicesController.java
@@ -0,0 +1,21 @@
+package InputDeviceTesting.uantwerpen.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by stuerjoris on 1/12/15.
+ */
+
+@Controller
+@RequestMapping("/devices/")
+
+public class DevicesController
+{
+    @RequestMapping(value = "devices", method = RequestMethod.GET)
+    public String report()
+    {
+        return "devices";
+    }
+}
